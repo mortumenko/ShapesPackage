58484de676b6f3426c65ee1b9aa0224d4a086127
mortumenko <sashkomortum@gmail.com>
2019-10-18 14:25:30 +0300

    Extended access level again

--- Sources/ShapesPackage/Sandbox.swift
+++ Sources/ShapesPackage/Sandbox.swift
@@ -12,6 +12,10 @@ let circlePerimetr = silentCircle.perimeter()
 
 class Sandbox {
     
+    public init() {
+        print("[\(self)] init")
+    }
+    
     public func startForCircles() {
         let silentCircle = Circle(radius: 3.5)
         let circlePerimeter = silentCircle.perimeter()
